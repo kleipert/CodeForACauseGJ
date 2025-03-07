f94d5ad6e10d7dc906564d56ffb949dd8743f57c
Kevin <kleipert@web.de>
2025-03-07 16:05:22 +0100

    [ADD] Laser Beam

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -1,37 +1,39 @@
 using System.Collections;
-using Enemies;
 using Managers;
 using UnityEngine;
 
-public class EnemyStats : MonoBehaviour
+namespace Enemies
 {
-    [SerializeField] private float _health = 250f;
-    private EnemyAnimations _enemyAnimations;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class EnemyStats : MonoBehaviour
     {
-        _enemyAnimations = GetComponent<EnemyAnimations>();
-    }
+        [SerializeField] private float _health = 250f;
+        private EnemyAnimations _enemyAnimations;
     
-    // Update is called once per frame
-    void Update()
-    {
-        if (_health <= 0)
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            _enemyAnimations.DeathAnimation();
-            StartCoroutine(nameof(DestroyObject));
+            _enemyAnimations = GetComponent<EnemyAnimations>();
         }
-    }
     
-    public void ReceiveDamageEnemy(float damage)
-    {
-        _health -= damage * PlayerManager.Instance.GetPlayerVelocity();
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (_health <= 0)
+            {
+                _enemyAnimations.DeathAnimation();
+                StartCoroutine(nameof(DestroyObject));
+            }
+        }
+    
+        public void ReceiveDamageEnemy(float damage)
+        {
+            _health -= damage * PlayerManager.Instance.GetPlayerVelocity();
+        }
 
-    IEnumerator DestroyObject()
-    {
-        yield return new WaitForSeconds(6f);
-        Destroy(gameObject);
+        IEnumerator DestroyObject()
+        {
+            yield return new WaitForSeconds(6f);
+            Destroy(gameObject);
+        }
     }
 }
